a36c2b2e77422b56c74b784d2309b007de1db478
Tomasz Sikora <sikoratomaszit@gmail.com>
2024-03-25 20:33:42 +0100

    blogger

--- src/app/day4/page.tsx
+++ src/app/day4/page.tsx
@@ -7,6 +7,9 @@ export default function Day4() {
       <Link href="/day4/moderation">
         <p>moderation</p>
       </Link>
+      <Link href="/day4/blogger">
+        <p>blogger</p>
+      </Link>
     </>
   );
 }
